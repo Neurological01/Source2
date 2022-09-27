0a5c55b84ed7223e569b04baa4041ac7b3e4b67d
Neurological01 <113656521+Neurological01@users.noreply.github.com>
2022-09-27 18:12:14 +0800

    Create Source.lua

---
+++ Source.lua
@@ -0,0 +1,22 @@
+--for when people ask for scripts in a discord server
+
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "DUMBASS LOL";
+Text = "You are stupid fr just search online please";
+Duration = 9000000000000000000000000;
+})
+
+wait(2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Lol sorry";
+Text = "But you should google lmao";
+Duration = 9000000000000000000000000;
+})
+
+wait(3)
+game.StarterGui:SetCore("SendNotification", {
+Title = "L";
+Text = "Btw this is now stuck on your screen until you rejoin";
+Duration = 9000000000000000000000000;
+})
